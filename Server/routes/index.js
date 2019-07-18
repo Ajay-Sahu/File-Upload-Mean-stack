52f58cce171b116d965de91a35e67fbe3299a5c3
Ajay <artstrom.2013@gmail.com>
2019-07-18 10:28:53 +0530

    upadate

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -3,17 +3,10 @@ var router = express.Router();
 const files = require('../src/components/fileUpload/fileUploadRoutes')
 
 
-/* GET home page. */
-// router.get('/', function (req, res, next) {
-//   res.render('index', { title: 'Express' });
-
-// });
-
-
-// router.get('/files', files)
-
-// module.exports = router
 
 module.exports = (app) => {
+  //Test node 
+  app.get('/', (req, res) => { res.status(200).json({ Message: "Welcome to Node project !", }) }),
+
   app.use('/files', files)
 };
--- Server/src/components/fileUpload/fileUploadController.js
+++ Server/src/components/fileUpload/fileUploadController.js
@@ -12,7 +12,13 @@ fileUploadController.uploadFile = (req, res) => {
   upload.uploadfileFromMulter(req, res, (err, data) => {
     if (err) return res.status(400).send({ msg: err });
 
-    // console.log('Data: ', data)
+    console.log('Data: --- ', data)
+
+    filesModel.insertOne({ fileName: data.key, filePath: data.Location }, (err, data) => {
+      if (err) res.status(400).send({ msg: err });
+
+      console.log('Insert res: ', data)
+    })
 
   })
 
